756d67af2b98c3e08519c51daf00090d68b094e6
Joshua Batson <joshua.batson@czbiohub.org>
2019-02-20 17:50:39 -0800

    Added diffusion

---
+++ src/noise2self_sc/diffusion.py
@@ -0,0 +1,102 @@
+import magic
+import numpy as np
+from scipy.sparse import issparse
+from util import split_counts, normalize_rows, mse
+from matplotlib import pyplot as plt
+
+
+def sweep_time(diff_op, X1, X2, max_t):
+    t_range = np.arange(max_t)
+
+    losses = np.zeros(max_t)
+    best_t = 0
+    best_loss = mse(X2, 0)
+    denoised = X1
+    best_denoised = denoised
+
+    for t in t_range:
+        loss = mse(denoised, X2)
+        losses[t] = loss
+        if loss < best_loss:
+            best_loss = loss
+            best_t = t
+            best_denoised = denoised
+        denoised = diff_op.dot(denoised)
+
+    return best_denoised, best_t, t_range, losses
+
+
+def sweep_time_per_gene(diff_op, X1, X2, max_t):
+
+    t_range = np.arange(max_t)
+
+    gene_losses = np.zeros((max_t, X1.shape[1]))
+
+    denoised = X1
+    best_denoised = denoised
+
+    for t in t_range:
+        gene_losses[t] = ((denoised - X2)**2).sum(axis=0)
+
+        new_best_idx = (np.min(gene_losses, axis=0) == gene_losses[t])
+        best_denoised[:, new_best_idx] = denoised[:, new_best_idx]
+
+
+        denoised = diff_op.dot(denoised)
+
+    best_t = np.argmin(gene_losses, axis = 0)
+
+    return best_denoised, best_t, t_range, gene_losses
+
+
+def n2s_magic(adata, max_t=20, lazy_p = 0.5, plot=True, per_gene=False, verbose=True, **kwargs):
+
+    if adata.raw is None:
+        X = adata.X
+    else:
+        X = adata.raw.X
+
+    if issparse(X):
+        X = np.array(X.todense())
+
+    X1, X2 = split_counts(X)
+
+    median_counts = np.median(X.sum(axis=1))/2
+
+    X1 = np.sqrt(normalize_rows(X1, median_counts))
+    X2 = np.sqrt(normalize_rows(X2, median_counts))
+
+    magic_op = magic.MAGIC(**kwargs)
+    magic_op.fit(X1)
+
+    diff_op = np.array(magic_op.diff_op.todense())
+
+    diff_op = lazy_p * diff_op + (1 - lazy_p) * np.eye(diff_op.shape[0])
+
+    if per_gene:
+        best_denoised, best_t, t_range, losses = sweep_time_per_gene(diff_op, X1, X2, max_t)
+
+        if plot:
+            plt.hist(best_t, bins=20)
+            plt.xlabel("diffusion time")
+            plt.title("Optimal per-gene diffusion times")
+
+    else:
+        best_denoised, best_t, t_range, losses = sweep_time(diff_op, X1, X2, max_t)
+
+        if plot:
+            plt.plot(t_range, losses)
+            plt.xlabel("diffusion time")
+            plt.ylabel("Self-Supervised Loss")
+            plt.title("Sweep t")
+            plt.axvline(best_t, color='k', linestyle='--')
+
+
+    denoised_adata = adata.copy()
+    denoised_adata.X = best_denoised
+
+    if verbose:
+        return denoised_adata, best_t, t_range, losses
+
+    else:
+        return denoised_adata
---
+++ src/noise2self_sc/util.py
@@ -0,0 +1,31 @@
+import numpy as np
+import scanpy as sc
+
+def normalize_rows(X, n_counts=None):
+    if n_counts is None:
+        n_counts = np.median(X.sum(axis=1))
+
+    return X / X.sum(axis=1, keepdims=True) * n_counts
+
+def split_counts(X):
+    X = X.astype(np.int)
+    X1 = np.random.binomial(X, 0.5)
+    X2 = X - X1
+    return X1, X2
+
+def mse(x, y):
+    return ((x - y)**2).mean()
+
+def standard_scanpy(adata):
+
+    if adata.raw is None:
+        adata.raw = adata
+    sc.pp.normalize_per_cell(adata)
+    sc.pp.sqrt(adata)
+    sc.pp.pca(adata)
+    sc.pp.neighbors(adata, n_neighbors=10, n_pcs=25)
+    sc.tl.umap(adata)
+
+    sc.tl.louvain(adata)
+
+    return adata
