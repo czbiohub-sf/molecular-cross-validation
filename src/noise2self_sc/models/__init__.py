e80fbd0b30446031234a0fc29b3a55289a6fae2e
James Webber <james.webber@czbiohub.org>
2019-03-20 13:22:41 -0700

    Made unet available from the models module.

--- src/noise2self_sc/models/__init__.py
+++ src/noise2self_sc/models/__init__.py
@@ -2,3 +2,4 @@
 
 from noise2self_sc.models.autoencoder import CountAutoencoder
 from noise2self_sc.models.resnet import ResidualEncoder
+from noise2self_sc.models.unet_autoencoder import UnetAutoencoder
