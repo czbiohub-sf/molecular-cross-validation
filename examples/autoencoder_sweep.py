6db6c95ce63eaf62825d523a292de337a8d767ec
James Webber <james.webber@czbiohub.org>
2019-06-17 16:21:56 -0700

    Fixed some bugs that showed up when I tried to actually run this script

--- examples/autoencoder_sweep.py
+++ examples/autoencoder_sweep.py
@@ -21,16 +21,18 @@ from noise2self_sc.train.aggmo import AggMo
 
 parser = argparse.ArgumentParser()
 
-run_group = parser.add_argument_group("run", help="Per-run parameters")
+run_group = parser.add_argument_group("run", description="Per-run parameters")
 run_group.add_argument("--seed", type=int, required=True)
 run_group.add_argument("--gpu", type=int, required=True)
 
-data_group = parser.add_argument_group("data", help="Input and output parameters")
+data_group = parser.add_argument_group(
+    "data", description="Input and output parameters"
+)
 data_group.add_argument("--workset", type=pathlib.Path, required=True)
 data_group.add_argument("--training", type=pathlib.Path, required=True)
 data_group.add_argument("--output_dir", type=pathlib.Path, required=True)
 
-model_group = parser.add_argument_group("model", help="Model parameters")
+model_group = parser.add_argument_group("model", description="Model parameters")
 model_group.add_argument("--layers", nargs="+", type=int, default=(128,))
 model_group.add_argument("--max_bottleneck", type=int, default=128)
 model_group.add_argument("--n2s", action="store_true", help="self-supervised training")
@@ -72,8 +74,8 @@ umis_X = torch.from_numpy(umis_X).to(torch.float)
 umis_Y = torch.from_numpy(umis_Y).to(torch.float)
 
 n_features = umis_X.shape[-1]
-bottlenecks = [2**i for i in range(1, args.max_bottleneck + 1)]
-bottlenecks.extend(3 * b // 2 for v in bottlenecks[:-1])
+bottlenecks = [2 ** i for i in range(1, args.max_bottleneck + 1)]
+bottlenecks.extend(3 * b // 2 for b in bottlenecks[:-1])
 bottlenecks.sort()
 
 if args.pois:
@@ -98,15 +100,12 @@ batch_size = len(sample_indices)
 
 def test_bottlenecks(bs, m_factory, opt_factory, criterion, train_data, val_data):
     b_results = dict()
-    scheduler_kw = {
-        "t_max": 128, "eta_min": args.learning_rate / 100.0, "factor": 1.0
-    }
+    scheduler_kw = {"t_max": 128, "eta_min": args.learning_rate / 100.0, "factor": 1.0}
 
     for b in bs:
         logger.info(f"testing bottleneck width {b}")
         model = m_factory(b)
-        optimizer = opt_factory()
-
+        optimizer = opt_factory(model)
 
         b_results[b] = n2s.train.train_until_plateau(
             model,
@@ -137,7 +136,7 @@ with torch.cuda.device(device):
         dropout_rate=args.dropout,
     )
 
-    optimizer_factory = lambda: AggMo(
+    optimizer_factory = lambda model: AggMo(
         model.parameters(),
         lr=args.learning_rate,
         betas=[0.0, 0.9, 0.99],
