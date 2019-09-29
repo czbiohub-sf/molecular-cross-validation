3b11b53fb21ddfde5bd4a3d18369269dfefe05dd
James Webber <james.webber@czbiohub.org>
2019-09-28 19:24:57 -0700

    Adding separate scripts for MCV training utils and script

---
+++ src/molecular_cross_validation/scripts/mcv_autoencoder_sweep.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python
+
+import argparse
+import logging
+import pathlib
+import pickle
+import time
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as func
+from torch.utils.data import DataLoader
+
+import molecular_cross_validation as mcv
+import molecular_cross_validation.train
+
+from molecular_cross_validation.models.autoencoder import CountAutoencoder
+from molecular_cross_validation.train.aggmo import AggMo
+
+import molecular_cross_validation.util as ut
+
+import molecular_cross_validation.train.mcv_train as mcvt
+
+
+def mse_loss_cpu(y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
+    y_pred = y_pred.detach().cpu()
+
+    return func.mse_loss(y_pred, y_true)
+
+
+def poisson_nll_loss_cpu(y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
+    y_pred = y_pred.detach().cpu()
+
+    return func.poisson_nll_loss(y_pred, y_true)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    run_group = parser.add_argument_group("run", description="Per-run parameters")
+    run_group.add_argument("--seed", type=int, required=True)
+    run_group.add_argument(
+        "--data_split", type=float, default=0.9, help="Split for self-supervision"
+    )
+    run_group.add_argument("--gpu", type=int, required=True)
+
+    data_group = parser.add_argument_group(
+        "data", description="Input and output parameters"
+    )
+    data_group.add_argument("--dataset", type=pathlib.Path, required=True)
+    data_group.add_argument("--output_dir", type=pathlib.Path, required=True)
+
+    model_group = parser.add_argument_group("model", description="Model parameters")
+
+    loss_group = model_group.add_mutually_exclusive_group(required=True)
+    loss_group.add_argument(
+        "--mse",
+        action="store_const",
+        const="mse",
+        dest="loss",
+        help="mean squared error",
+    )
+    loss_group.add_argument(
+        "--pois",
+        action="store_const",
+        const="pois",
+        dest="loss",
+        help="poisson likelihood",
+    )
+
+    model_group.add_argument(
+        "--layers",
+        nargs="+",
+        type=int,
+        metavar="L",
+        default=[128],
+        help="Layers in the input/output networks",
+    )
+    model_group.add_argument(
+        "--max_bottleneck",
+        type=int,
+        default=7,
+        metavar="B",
+        help="max bottleneck (log2)",
+    )
+    model_group.add_argument(
+        "--learning_rate", type=float, default=0.1, metavar="LR", help="learning rate"
+    )
+    model_group.add_argument(
+        "--dropout", type=float, default=0.0, metavar="P", help="dropout probability"
+    )
+
+    args = parser.parse_args()
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler())
+
+    logger.info(f"torch version {torch.__version__}")
+
+    dataset_name = args.dataset.parent.name
+    output_file = (
+        args.output_dir
+        / f"{dataset_name}_n2s_autoencoder_{args.loss}_{args.seed}.pickle"
+    )
+
+    logger.info(f"writing output to {output_file}")
+
+    seed = sum(map(ord, f"biohub_{args.seed}"))
+    random_state = np.random.RandomState(seed)
+
+    device = torch.device(f"cuda:{args.gpu}")
+
+    torch.backends.cudnn.deterministic = True
+    torch.manual_seed(seed)
+
+    with open(args.dataset, "rb") as f:
+        true_means, true_counts, umis = pickle.load(f)
+
+    n_features = umis.shape[-1]
+
+    bottlenecks = [2 ** i for i in range(args.max_bottleneck + 1)]
+    bottlenecks.extend(3 * b // 2 for b in bottlenecks[1:-1])
+    bottlenecks.sort()
+
+    logger.info(f"testing bottlenecks {bottlenecks}")
+
+    if max(bottlenecks) > max(args.layers):
+        raise ValueError("Max bottleneck width is larger than your network layers")
+
+    rec_loss = np.empty(len(bottlenecks), dtype=float)
+    gt0_loss = np.empty_like(rec_loss)
+
+
+    if args.loss == "mse":
+        exp_means = ut.expected_sqrt(true_means * umis.sum(1, keepdims=True))
+        exp_means = torch.from_numpy(exp_means).to(torch.float)
+
+        loss_fn = nn.MSELoss()
+        normalization = "sqrt"
+        input_t = torch.sqrt
+        eval0_fn = mse_loss_cpu
+    else:
+        assert args.loss == "pois"
+        exp_means = true_means * umis.sum(1, keepdims=True)
+        exp_means = torch.from_numpy(exp_means).to(torch.float)
+
+        loss_fn = nn.PoissonNLLLoss()
+        normalization = "log1p"
+        input_t = torch.log1p
+        eval0_fn = poisson_nll_loss_cpu
+
+    model_factory = lambda bottleneck: CountAutoencoder(
+        n_input=n_features,
+        n_latent=bottleneck,
+        layers=args.layers,
+        use_cuda=True,
+        dropout_rate=args.dropout,
+    )
+
+    optimizer_factory = lambda m: AggMo(
+        m.parameters(), lr=args.learning_rate, betas=[0.0, 0.9, 0.99], weight_decay=1e-7
+    )
+
+    scheduler_kw = {"t_max": 256, "eta_min": args.learning_rate / 100.0, "factor": 1.0}
+
+    full_train_losses = []
+    full_val_losses = []
+
+    with torch.cuda.device(device):
+        umis = torch.from_numpy(umis).to(torch.float).to(device)
+
+        sample_indices = random_state.permutation(umis.size(0))
+        n_train = int(0.875 * umis.size(0))
+
+        train_dl, val_dl = mcvt.split_mcv_dataset(
+            umis,
+            batch_size=len(sample_indices),
+            indices=sample_indices,
+            n_train=n_train,
+            split_prob=args.data_split
+        )
+
+        t0 = time.time()
+
+        for j, b in enumerate(bottlenecks):
+            logger.info(f"testing bottleneck width {b}")
+
+            model = model_factory(b)
+            optimizer = optimizer_factory(model)
+
+            full_train_loss, full_val_loss = mcvt.mcv_train_until_plateau(
+                model,
+                loss_fn,
+                optimizer,
+                train_dl,
+                val_dl,
+                input_t=input_t,
+                min_cycles=3,
+                threshold=0.001,
+                scheduler_kw=scheduler_kw,
+            )
+
+            full_train_losses.append(full_train_loss)
+            full_val_losses.append(full_val_loss)
+
+            logger.debug(f"finished {b} after {time.time() - t0} seconds")
+
+            rec_loss[j] = full_train_loss[-1]
+            gt0_loss[j] = eval0_fn(model(input_t(umis)), exp_means)
+
+    results = {
+        "dataset": dataset_name,
+        "method": "autoencoder",
+        "loss": args.loss,
+        "normalization": normalization,
+        "param_range": bottlenecks,
+        "rec_loss": rec_loss,
+        "gt0_loss": gt0_loss,
+        "full_train_losses": full_train_losses,
+        "full_val_losses": full_val_losses,
+    }
+
+    with open(output_file, "wb") as out:
+        pickle.dump(results, out)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/molecular_cross_validation/train/mcv_train.py
@@ -0,0 +1,208 @@
+import itertools
+
+from typing import Callable, Sequence, Tuple
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+from torch.nn.utils.clip_grad import clip_grad_norm_
+from torch.optim import Optimizer
+from torch.utils.data import DataLoader, TensorDataset, SubsetRandomSampler
+
+from molecular_cross_validation.train.cosine_scheduler import CosineWithRestarts
+
+Transform = Callable[[torch.Tensor], torch.Tensor]
+
+
+class MCVDataLoader(DataLoader):
+    """A version of the standard DataLoader that generates a new noise2self split every
+    time it is iterated on. The dataset passed in should be an instance of TensorDataset
+    and contain one tensor of UMI counts. Any transformation(s) of the data must be done
+    downstream of this class.
+    """
+
+    def __init__(self, dataset, split_prob=0.9, **kwargs):
+        super(MCVDataLoader, self).__init__(dataset=dataset, **kwargs)
+
+        self.b_dist = torch.distributions.Binomial(
+            self.dataset.tensors[0].cuda(), probs=split_prob
+        )
+
+    def __iter__(self):
+        x_data = self.b_dist.sample()
+        y_data = self.b_dist.total_count - x_data
+
+        for indices in iter(self.batch_sampler):
+            yield (x_data[indices], y_data[indices]) + tuple(
+                d[indices] for d in self.dataset.tensors[1:]
+            )
+
+
+def split_mcv_dataset(
+    *xs: torch.Tensor,
+    batch_size: int,
+    indices: np.ndarray = None,
+    n_train: int = None,
+    split_prob: float = 0.9,
+):
+    if indices is None:
+        indices = np.random.permutation(xs[0].shape[0])
+
+    if n_train is None:
+        n_train = int(0.875 * xs[0].shape[0])
+
+    ds = TensorDataset(xs[0], *xs[1:])
+
+    training_dl = MCVDataLoader(
+        dataset=ds,
+        batch_size=batch_size,
+        sampler=SubsetRandomSampler(indices[:n_train]),
+        split_prob=split_prob,
+    )
+
+    validation_dl = MCVDataLoader(
+        dataset=ds,
+        batch_size=batch_size,
+        sampler=SubsetRandomSampler(indices[n_train:]),
+        split_prob=split_prob,
+    )
+
+    return training_dl, validation_dl
+
+
+def mcv_train_epoch(
+    model: nn.Module,
+    criterion: nn.Module,
+    optim: Optimizer,
+    data_loader: DataLoader,
+    input_t: Transform,
+    clip_norm: float = None,
+):
+    """Iterate through training data, compute losses and take gradient steps
+
+    :param model: a torch Module that can take input data and return the prediction
+    :param criterion: a loss function
+    :param optim: a torch Optimizer
+    :param data_loader: training dataset. Should produce a tuple of tensors: the first
+                        is used as input and the last is the target. If the tuple has
+                        only one element then it's used for both
+    :param input_t: Transformation to apply to the input
+    :param clip_norm: clip gradient norm to a given absolute value
+    :return: total loss for the epoch, averaged over the number of batches
+    """
+    total_epoch_loss = 0.0
+
+    for data in data_loader:
+        y = model(input_t(data[0]))
+        loss = criterion(y, data[1])
+
+        total_epoch_loss += loss.data.item()
+
+        optim.zero_grad()
+        loss.backward()
+        if clip_norm is not None:
+            clip_grad_norm_(model.parameters(), clip_norm)
+        optim.step()
+
+    return total_epoch_loss / len(data_loader)
+
+
+def mcv_evaluate_epoch(
+    model: nn.Module,
+    criterion: nn.Module,
+    data_loader: DataLoader,
+    input_t: Transform,
+    eval_i: Sequence[int],
+):
+    """Iterate through test data and compute losses
+
+    :param model: a torch Module that can take input data and return the prediction
+    :param criterion: a loss function
+    :param data_loader: validation dataset. Should produce a tuple of tensors: the first
+                        is used as input and the last is the target. If the tuple has
+                        only one element then it's used for both
+    :param input_t: Transformation to apply to the input
+    :param eval_i: Index into the DataLoader tuple for evaluation
+    :return: total loss for the epoch, averaged over the number of batches
+    """
+    total_epoch_loss = 0.0
+
+    for data in data_loader:
+        y = model(input_t(data[0]))
+        loss = criterion(y, *(data[i] for i in eval_i))
+
+        total_epoch_loss += loss.data.item()
+
+    return total_epoch_loss / len(data_loader)
+
+
+def mcv_train_until_plateau(
+    model: nn.Module,
+    training_loss: nn.Module,
+    optim: Optimizer,
+    training_data: DataLoader,
+    validation_data: DataLoader,
+    input_t: Transform,
+    min_cycles: int = 3,
+    threshold: float = 0.01,
+    scheduler_kw: dict = None,
+    verbose: bool = False,
+) -> Tuple[list, list]:
+    assert 0.0 <= threshold < 1.0
+
+    if scheduler_kw is None:
+        scheduler_kw = dict()
+
+    train_loss = []
+    val_loss = []
+
+    scheduler = CosineWithRestarts(optim, **scheduler_kw)
+    best = np.inf
+    rel_epsilon = 1.0 - threshold
+    neg_epsilon = 1.0 + threshold
+    cycle = 0
+
+    for epoch in itertools.count():
+        optim.zero_grad()  # just make sure things are zeroed before train loop
+        model.train()
+
+        train_loss.append(
+            mcv_train_epoch(
+                model=model,
+                criterion=training_loss,
+                optim=optim,
+                data_loader=training_data,
+                input_t=input_t,
+                clip_norm=100.0,
+            )
+        )
+
+        model.eval()
+        val_loss.append(
+            mcv_evaluate_epoch(
+                model=model,
+                criterion=training_loss,
+                data_loader=validation_data,
+                input_t=input_t,
+                eval_i=[1],
+            )
+        )
+
+        scheduler.step()
+        if scheduler.starting_cycle:
+            if verbose:
+                print(
+                    f"[epoch {epoch:03d}]  average training loss: {train_loss[-1]:.5f}"
+                )
+            cycle += 1
+
+            if 0 <= val_loss[-1] < best * rel_epsilon:
+                best = val_loss[-1]
+            elif 0 > val_loss[-1] and val_loss[-1] < best * neg_epsilon:
+                best = val_loss[-1]
+            elif cycle >= min_cycles:
+                break
+
+    return train_loss, val_loss
